a6ed7356ff8388c283428c08545974c8972f7ae0
Chiru B <33750251+itsezc@users.noreply.github.com>
2022-01-11 23:09:00 +0000

    chore: replace NPM with bonnie

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 module.exports = {
-	purge: {
-		mode: 'all',
-		content: [
-			'./src/**/*.rs',
-			'./index.html',
-			'./src/**/*.html',
-			'./src/**/*.css',
-		],
-	},
+	content: [
+		'./src/**/*.{html,rs}',
+		'./index.html'
+	],
 	theme: {},
 	variants: {},
 	plugins: []
