5412bf4045fde33fbb3b7856f1a8d65e0d3096b4
Chiru B <33750251+itsezc@users.noreply.github.com>
2022-01-16 03:10:12 +0000

    implement: TailwindCSS (Yew) extractor

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
-// mod xr;
-// mod front;
-
 use yew::prelude::*;
 
 #[function_component(App)]
-fn app() -> Html 
+fn app() -> Html
 {
-	html! 
+	html!
 	{
-		<h1 class={classes!("text-xl", "text-blue-500")}>{ "Hello World xD xD" }</h1>
+		<h1 class={classes!("text-3xl", "text-blue-500")}>{ "Hello World xD xD" }</h1>
 	}
 }
 
 fn main()
 {
 	yew::start_app::<App>();
-
-	// xr::run_game();
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 module.exports = {
-	content: [
-		'./src/**/*.{html,rs}',
-		'./index.html'
-	],
+	content: {
+		files: [
+			'./src/**/*.rs',
+			'./index.html'
+		],
+		extract: {
+			rs: (content) => {
+				const customRegex = /(?<=classes!\((?:"[^"]*",\s*)*")[^"]*(?=")/g;
+
+				const result = content.match(customRegex) || [];
+
+				let results = [
+					...result
+				].flat().filter((v) =>v !== undefined);
+
+				return results;
+			}
+		}
+	},
 	theme: {},
 	variants: {},
 	plugins: []
